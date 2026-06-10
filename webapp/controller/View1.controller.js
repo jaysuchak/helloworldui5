e291dbe58ea503f0600e9a26110b2533c25a821a
I756811 <jaydeep.suchak01@sap.com>
2026-06-10 19:31:27 +0530

    refactor: update button press handler to use MessageToast for Hello World message

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,14 +1,14 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox"
-], (Controller, MessageBox) => {
+    "sap/m/MessageToast"
+], (Controller, MessageToast) => {
     "use strict";
 
     return Controller.extend("sap.btp.helloworldui5.controller.View1", {
         onInit() {
         },
-        onPress() {
-            MessageBox.alert("You have been alerted!");
+        onHelloWorldPress() {
+            MessageToast.show("Hello World");
         }
     });
 });
