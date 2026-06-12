afaa0b69167befe9f22b14be5f979a6e4492d1d5
Jaydeep Suchak <jaydeep.suchak01@sap.com>
2026-06-12 11:03:31 +0000

    toast message changed

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,7 +8,7 @@ sap.ui.define([
         onInit() {
         },
         onHelloWorldPress() {
-            MessageToast.show("Hello World");
+            MessageToast.show("Hello Ramesh!!");
         }
     });
 });
